3430bb73b92e250958906169d1d07742a0ee3dfe
Gledson84 <gledsonpinheiro84@gmail.com>
2024-09-24 00:07:34 -0300

    Update Avaliação_Aula_01.py

--- Avaliação_Aula_01.py
+++ Avaliação_Aula_01.py
@@ -2,4 +2,4 @@
 base_retangulo = float(input('Informe o comprimento da base: '))
 altura_retangulo = float(input('Informe o comprimento da altura: '))
 area_retangulo = base_retangulo * altura_retangulo
-print(f'A área do retângulo é: {area_retangulo} cm')
+print(f'A área do retângulo é: {area_retangulo} centimetros quadrados')
